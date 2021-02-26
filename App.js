eb894f229e083cb0e447f8bab74c829cf9e37825
MatheusAlves <m-alves7@hotmail.com>
2021-02-26 19:13:40 -0300

    Entrega

--- App.js
+++ App.js
@@ -3,10 +3,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const numerosPossiveis = Array.from(Array(60).keys());
+  let numeros = [];
+  for (let i = 0; i < 6; i++) {
+    let n = Math.floor(Math.random() * (60 - i));
+    numeros = [...numeros, numerosPossiveis[n]];
+    numerosPossiveis.splice(n,1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{numeros.join(' - ')}</Text>
     </View>
   );
 }
